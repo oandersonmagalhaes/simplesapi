622e41d2b0e5e192318cf7a126547988a42872a7
Anderson Magalhaes <andersonmagalhaes.dev@gmail.com>
2024-05-30 21:47:58 -0300

    refactor: update the initial config Updating the initial config to include the lifespan and how to access database and other application functionalities.

--- simplesapi/__init__.py
+++ simplesapi/__init__.py
@@ -2,4 +2,8 @@
 SimplesAPI
 """
 
-from .app import SimplesAPI as SimplesAPI
+from .app import (SimplesAPI as SimplesAPI, 
+                  SimplesConfig as SimplesConfig)
+from .lifespan import (
+    database as database
+)
--- simplesapi/app.py
+++ simplesapi/app.py
@@ -3,21 +3,22 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 
+from simplesapi import lifespan, settings
 from simplesapi.auto_routing import register_routes
 
-class _Simples(BaseModel):
-    verbose: bool
+class SimplesConfig(BaseModel):
+    verbose: Optional[bool] = Field(default=False)
     base_path: Optional[str] = Field(default="/")
-    redis_conn: Optional[str] = Field(default=None)
-    postgres_conn: Optional[str] = Field(default=None)
-    mysql_conn: Optional[str] = Field(default=None)
+    routes_path: Optional[str] = Field(default="/routes")
+    redis_url: Optional[str] = Field(default=settings.SIMPLESAPI_REDIS_URL)
+    redis_ssl: Optional[bool] = Field(default=settings.SIMPLESAPI_REDIS_SSL)
+    database_connection_string: Optional[str] = Field(default=settings.SIMPLESAPI_DB_CONN)
 
 class SimplesAPI(FastAPI):
-    def __init__(self, title="SimplesAPI", version="0.1.0", routes_path=None, base_path=None, redis_conn=None, postgres_conn=None, mysql_conn=None, verbose=False):
-        super().__init__(title=title, version=version)
-        self.simples = _Simples(verbose=verbose)
-        if routes_path:
-            register_routes(self,routes_path)
+    def __init__(self, simples=SimplesConfig(), **kwargs):
+        self.simples = simples
+        super().__init__(lifespan=lifespan, **kwargs)
+        if simples.routes_path:
+            register_routes(self,simples.routes_path)
 
-    def validate_simples(self):
-        ...
+        
