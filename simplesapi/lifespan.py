765a9ea14b7d12fb333a76116bf87f1897caaefa
Anderson Magalhaes <andersonmagalhaes.dev@gmail.com>
2024-05-31 00:10:46 -0300

    fix: circular dependencies

--- simplesapi/lifespan.py
+++ simplesapi/lifespan.py
@@ -1,9 +1,8 @@
 from contextlib import asynccontextmanager
-from simplesapi.app import SimplesAPI
 
 database = None
 
 @asynccontextmanager
-async def lifespan(app: SimplesAPI):
+async def lifespan(app):
     database = {}
     yield
