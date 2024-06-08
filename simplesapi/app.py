e0d299a79bc4dfd9ffed3178b0f3b1b6fc3322fc
Anderson Magalhaes <andersonmagalhaes.dev@gmail.com>
2024-06-08 14:08:26 -0300

    feature: expose the routes_path config

--- simplesapi/app.py
+++ simplesapi/app.py
@@ -9,14 +9,14 @@ from simplesapi.auto_routing import register_routes
 class SimplesConfig(BaseModel):
     verbose: Optional[bool] = Field(default=False)
     base_path: Optional[str] = Field(default="/")
-    routes_path: Optional[str] = Field(default="/routes")
+    routes_path: Optional[str] = Field(default="routes")
     redis_url: Optional[str] = Field(default=settings.SIMPLESAPI_REDIS_URL)
     redis_ssl: Optional[bool] = Field(default=settings.SIMPLESAPI_REDIS_SSL)
     database_connection_string: Optional[str] = Field(default=settings.SIMPLESAPI_DB_CONN)
 
 class SimplesAPI(FastAPI):
-    def __init__(self, *args, **kwargs):
-        self.simples = SimplesConfig(**kwargs)
+    def __init__(self, routes_path=None, *args, **kwargs):
+        self.simples = SimplesConfig(routes_path=routes_path,**kwargs)
         super().__init__(lifespan=lifespan.lifespan, *args, **kwargs)
         if self.simples.routes_path:
             register_routes(self,self.simples.routes_path)
