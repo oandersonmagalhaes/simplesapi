16aacc37309e923e59512809a356321a98221de8
Anderson Magalhaes <andersonmagalhaes.dev@gmail.com>
2024-06-08 14:07:07 -0300

    feature: adding internal logger

---
+++ simplesapi/internal_logger.py
@@ -0,0 +1,11 @@
+import logging
+
+
+def simplesapi_internal_logger():
+    logger = logging.getLogger("SimplesAPI")
+    logger.setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    log_formatter = logging.Formatter("\033[96mx%(levelname)s:     %(name)s:\033[0m %(message)s")
+    stream_handler.setFormatter(log_formatter)
+    logger.addHandler(stream_handler)
+    return logger
