e05afab1a0b0e47d447514c8782e2c223db80769
Anderson Magalhaes <andersonmagalhaes.dev@gmail.com>
2024-06-13 23:39:45 -0300

    feature: update initializing

--- simplesapi/app.py
+++ simplesapi/app.py
@@ -1,4 +1,3 @@
-
 from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
@@ -6,18 +5,25 @@ from pydantic import BaseModel, Field
 from simplesapi import lifespan, settings
 from simplesapi.auto_routing import register_routes
 
+
 class SimplesConfig(BaseModel):
     verbose: Optional[bool] = Field(default=False)
     base_path: Optional[str] = Field(default="/")
     routes_path: Optional[str] = Field(default="routes")
     redis_url: Optional[str] = Field(default=settings.SIMPLESAPI_REDIS_URL)
     redis_ssl: Optional[bool] = Field(default=settings.SIMPLESAPI_REDIS_SSL)
-    database_connection_string: Optional[str] = Field(default=settings.SIMPLESAPI_DB_CONN)
+    database_connection_string: Optional[str] = Field(
+        default=settings.SIMPLESAPI_DB_CONN
+    )
+
 
 class SimplesAPI(FastAPI):
-    def __init__(self, routes_path=None, *args, **kwargs):
-        self.simples = SimplesConfig(routes_path=routes_path,**kwargs)
+    def __init__(
+        self, routes_path=None, redis_url=None, redis_ssl=None, *args, **kwargs
+    ):
+        self.simples = SimplesConfig(
+            routes_path=routes_path, redis_url=redis_url, redis_ssl=redis_ssl, **kwargs
+        )
         super().__init__(lifespan=lifespan.lifespan, *args, **kwargs)
         if self.simples.routes_path:
-            register_routes(self,self.simples.routes_path)
-
+            register_routes(self, self.simples.routes_path)
