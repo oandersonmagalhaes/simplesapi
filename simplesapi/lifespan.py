843c1c05815ea354676a3bb4aea9fd106cd2083e
Anderson Magalhaes <andersonmagalhaes.dev@gmail.com>
2024-06-16 21:07:50 -0300

    feature: configuring cache

--- simplesapi/lifespan.py
+++ simplesapi/lifespan.py
@@ -1,6 +1,8 @@
 from contextlib import asynccontextmanager
 import logging
 from urllib.parse import urlparse
+
+import redis
 from databases import Database
 
 
@@ -10,12 +12,13 @@ logger = logging.getLogger("SimplesAPI")
 @asynccontextmanager
 async def lifespan(app):
     await configure_database(app=app)
-    app.cache = {}
+    await configure_cache(app=app)
     yield
     await close_database(app=app)
+    await close_cache(app=app)
 
 
-async def configure_database(app) -> Database:
+async def configure_database(app) -> None:
     if app.simples.database_url:
         database_info = extract_db_info(app.simples.database_url)
         logger.info(
@@ -25,6 +28,19 @@ async def configure_database(app) -> Database:
         await app.database.connect()
 
 
+async def configure_cache(app) -> None:
+    if app.simples.cache_url:
+        redis_info = extract_db_info(app.simples.cache_url)
+        logger.info(
+            f"Configuring cache | Host: {redis_info['host']}"
+        )
+        redis_conn = redis.ConnectionPool.from_url(
+            app.simples.cache_url, encoding="utf-8", decode_responses=True
+        )
+        app.cache = redis.Redis(connection_pool=redis_conn)
+    else:
+        app.cache = None
+
 
 def extract_db_info(db_url: str) -> dict:
     parsed_url = urlparse(db_url)
@@ -32,6 +48,8 @@ def extract_db_info(db_url: str) -> dict:
     db_name = parsed_url.path.lstrip("/")  # Remove leading slash
 
     return {"host": host, "database": db_name}
+
+
 async def close_database(app) -> Database:
     if app.database:
         database_info = extract_db_info(app.simples.database_url)
@@ -39,4 +57,14 @@ async def close_database(app) -> Database:
             f"Closing database | Host: {database_info['host']} | Database: {database_info['database']}"
         )
         await app.database.close()
+
+
+async def close_cache(app) -> Database:
+    if app.cache:
+        redis_info = extract_db_info(app.simples.redis_url)
+        logger.info(
+            f"Closing database | Host: {redis_info['host']} | Database: {redis_info['database']}"
+        )
+        await app.cache.close()
+
 
